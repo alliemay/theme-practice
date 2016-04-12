74a86e812c856356981c9e00ff4387f2078e401c
alliemay <allison@cultiverse.com>
2016-04-12 09:16:00 -0700

    Added files via upload

---
+++ footer.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Sassy_Alliemay
+ */
+
+?>
+
+	</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="site-info">
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'alliemaysass' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'alliemaysass' ), 'WordPress' ); ?></a>
+			<span class="sep"> | </span>
+			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'alliemaysass' ), 'alliemaysass', '<a href="http://allisonrolls.com" rel="designer">Alliemay</a>' ); ?>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
